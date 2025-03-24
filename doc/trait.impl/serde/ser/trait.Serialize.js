00d2bd1356e4d8a26f219f41b9ba5789247c4c7d
David Skyberg <davidskyberg@gmail.com>
2025-03-24 15:25:30 -0400

    docs

---
+++ doc/trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["pythagoras",[["impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"pythagoras/right_angle/struct.RightAngle.html\" title=\"struct pythagoras::right_angle::RightAngle\">RightAngle</a>"],["impl <a class=\"trait\" href=\"serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"pythagoras/right_angle/struct.RightAngleInput.html\" title=\"struct pythagoras::right_angle::RightAngleInput\">RightAngleInput</a>"]]],["serde",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[565,13]}
